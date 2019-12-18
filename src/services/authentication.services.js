1670abd3cc7fde94dbf5344f011cb50ddbb73ff6
alexandre cardoso <alexandre.cardoso.dev@gmail.com>
2019-12-18 22:03:03 +0100

    WIP login and form

---
+++ src/services/authentication.services.js
@@ -0,0 +1,32 @@
+const HTTP_BAD_REQUEST = 400;
+
+async function fetchAuthenticate(data) {
+    return new Promise(async (resolve, reject) => {
+        const url = 'http://localhost:4000/api/login';
+        const init = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+        const response = await fetch(url, init);
+
+        if (response.status >= HTTP_BAD_REQUEST) {
+            return reject({
+                result: await response.json(),
+                status: response.status
+            });
+        }
+
+        return resolve({
+            result: await response.json(),
+            status: response.status
+        });
+    });
+}
+
+export async function authenticate(data) {
+    const result = await fetchAuthenticate(data);
+    console.log(result);
+}
